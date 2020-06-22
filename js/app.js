f6b72d30c44a6ed8467ed71361d6f7b9051fc5f1
Glitch (hello-webpage) <none>
2020-06-22 17:41:47 +0000

    👠🎌 Checkpoint ./index.html:739936/314 ./js/app.js:739936/68 ./.glitch-assets:739936/361

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ import {parse} from "./moonshine.js";
 import * as Immer from "../lib/immer.min.js";
 
 console.log({immer, R, parse, dragula, heresy});
+
+const processScript = (script) => {
+  console.log(script);
+}
