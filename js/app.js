9c491cf75beb3318baf4421bca76096157ef4a1d
Glitch (hello-webpage) <none>
2020-06-22 06:24:36 +0000

    💋🗻 Checkpoint ./index.html:739936/62 ./js/app.js:739936/111 ./lib/heresy.min.js:739936/23

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
 import {parse} from "./moonshine.js";
-// import {heresy} from "../lib/heresy.min.js";
-import * as R from "../lib/ramda.min.js";
 
-console.log({R, parse});
+console.log({R, parse, dragula, heresy});
